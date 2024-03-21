9995e68f64b126e528fa010e9ad9241117a17e93
Charanjit55588 <charanjit55588@gmail.com>
2024-03-21 18:38:23 -0400

    hh

--- hello.text/Sunday.java
+++ hello.text/Sunday.java
@@ -1,19 +1,4 @@
-Hello Priya
-How r u you doing?
-
-3rd Third branch changes made by Priya
-changes to the third branch
-NO new changes
-hihi
-kkkkkkk
-
-Hello today
-try to create a conflict 
-hello Priya
-How r u ?
-
-
-        Java for Loop
+Java for Loop
         Java for loop is used to run a block of code for a certain number of times. The syntax of for loop is:
 
         for (initialExpression; testExpression; updateExpression) {
